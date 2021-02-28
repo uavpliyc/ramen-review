0ae384dcaf69d5d0add8b9eb31bae7ae7a599474
uavpliyc <uavpliyc@gmail.com>
2021-02-28 16:18:21 +0000

    finish

--- app/controllers/ramen_shops_controller.rb
+++ app/controllers/ramen_shops_controller.rb
@@ -1,9 +1,11 @@
 class RamenShopsController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
   def index
     @ramen_shops = RamenShop.all
   end
 
   def show
-    @ramen_shop = Ramen_shop.find(params[:id])
+    @ramen_shop = RamenShop.find(params[:id])
+    @review = Review.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_070922) do
+ActiveRecord::Schema.define(version: 2021_02_28_144159) do
 
   create_table "ramen_shops", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "ramen_shop_id"
+    t.string "content"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ramen_shop_id"], name: "index_reviews_on_ramen_shop_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
